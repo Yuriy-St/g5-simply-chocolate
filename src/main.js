45ad1333f837ddcf706dd6e390f5ad4356ccb90e
RuslanGSM <ruslangsm1553@gmail.com>
2023-03-28 15:48:02 +0300

    temp fix

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
+// slider
+$('.responsive').slick({
+  prevArrow: false,
+  nextArrow: false,
+  dots: true,
+  infinite: false,
+  speed: 300,
+  adaptiveHeight: true,
+  slidesToScroll: 1,
+  slidesToShow: 3,
+  variableWidth: false, 
 
-    function toggleModal() {
-        refs.modal.classList.toggle("is-hidden");
-    }
-})();
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
